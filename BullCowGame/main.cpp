6e42c2350659215281efba770536c946fec63905
rafal.manka <rafal@manka.info>
2018-07-22 20:36:53 +0400

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by rafal.manka on 22/07/2018.
+//  Copyright © 2018 Rafal Manka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
